0fbe621d135c687a70f38cd62cfa4ae6b2371f57
lip-realmax <56542376+lip-realmax@users.noreply.github.com>
2019-10-16 11:42:13 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y npm supervisor
+sudo npm install -g npm
+git clone --recursive https://github.com/lip-realmax/3dCamera.git
+cd 3dCamera
+sudo npm install
+sudo cp /home/pi/3dCamera/camera.conf /etc/supervisor/conf.d/camera.conf
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo service supervisor restart
